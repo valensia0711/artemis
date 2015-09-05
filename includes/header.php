638f58670691e3a2a5286ff66d0a8a054745e0e6
Chen Si <chensi@u.nus.edu>
2015-09-06 00:46:25 +0800

    Unclutter navbar
    Reduce width overflow by condensing some items to dropdown menu

--- includes/header.php
+++ includes/header.php
@@ -12,19 +12,27 @@
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li class="<?php echo ($page == "home" ? "active": "")?>"><a href="index">Home</a></li>
-                <li class="<?php echo ($page == "grab" ? "active": "")?>"><a href="grablist">Duty for Grab</a></li>
+                <li class="<?php echo ($page == "grab" ? "active": "")?>"><a href="grablist">Grab Duty</a></li>
                 <li class="<?php echo ($page == "announcement" ? "active": "")?>"><a href="announcement">Announcement</a></li>
                 <li class="<?php echo ($page == "report" ? "active": "")?>"><a href="report">Problem Report</a></li>
                 <li class="<?php echo ($page == "sign" ? "active": "")?>"><a href="sign">Sign In/Out</a></li>   
                 <li class="<?php echo ($page == "members" ? "active": "")?>"><a href="members">Members</a></li>   
                 <li class="<?php echo ($page == "guide" ? "active": "")?>"><a href="guide">Guide</a></li>   
-                <li class="<?php echo ($page == "postEOD" ? "active": "")?>"><a href="postEOD">Post EOD </a></li>
-                <li class="<?php echo ($page == "tracking" ? "active": "")?>"><a href="tracking">Money Tracking</a></li>
-                <li class="<?php echo ($page == "scheduleavailability" ? "active": "")?>"><a href="scheduleavailability">Schedule Availability</a></li>
+                <li class="<?php echo ($page == "postEOD" ? "active": "")?>"><a href="postEOD">EOD</a></li>
+                
+                
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li class="<?php echo ($page == "user" ? "active": "")?>"><a href="profile"><?php echo $_SESSION['user_name'];?></a></li>
-                <li><a href="logout">Logout</a></li>
+			<li class="dropdown <?php echo (($page == "tracking" || $page == "scheduleavailability" || $page == "user") ? "active": "")?>">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user" aria-hidden="true"> </span><?php echo $_SESSION['user_name'];?><span class="caret"></span></a>
+                <ul class="dropdown-menu">
+				<li><a href="profile"><span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span> My Profile</a></li>
+				<li><a href="scheduleavailability"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Schedule Availability</a></li>
+				<li><a href="tracking"><span class="glyphicon glyphicon-usd" aria-hidden="true"></span> Money Tracking</a></li>
+				<li role="separator" class="divider"></li>
+				<li><a href="logout">Logout</a></li>
+				</ul>
+			</li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
